7201d99cc9ff4ffb31c98da9975f08367e12a2f9
Robert Wagner <robert.wagner@octopus.com>
2024-03-15 16:31:39 +1000

    Disabled enforce admins

--- BranchProtections/main.tf
+++ BranchProtections/main.tf
@@ -28,7 +28,7 @@ resource "github_branch_protection" "main_branch" {
   for_each            = local.repositories
   repository_id       = each.value
   pattern             = "main"
-  enforce_admins      = true
+  enforce_admins      = false
   allows_deletions    = false
   allows_force_pushes = false
 
