0b2d19eaf1524dcfda3c79b8397ba1d3976d65bd
Robert Wagner <robert.wagner@octopus.com>
2024-03-15 14:27:32 +1000

    Initial config

---
+++ main.tf
@@ -0,0 +1,76 @@
+locals {
+  repositories = toset([
+    "dbup-mysql",
+    "dbup-sqlserver",
+  ])
+}
+
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">=5.40.0"
+    }
+  }
+  
+  cloud {
+    organization = "DbUp"
+
+    workspaces {
+      name = "GithubBranchProtections"
+    }
+  }
+}
+
+provider "github" {
+  owner = "DbUp"
+}
+
+resource "github_branch_protection" "main_branch" {
+  for_each            = local.repositories
+  repository_id       = each.value
+  pattern             = "main"
+  enforce_admins      = true
+  allows_deletions    = false
+  allows_force_pushes = false
+
+  restrict_pushes {
+    blocks_creations = true
+  }
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+    dismiss_stale_reviews           = true
+    require_last_push_approval      = true
+  }
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build", "Unit Tests"]
+  }
+}
+
+resource "github_branch_protection" "release_branches" {
+  for_each            = local.repositories
+  repository_id       = each.value
+  pattern             = "releases/**"
+  enforce_admins      = true
+  allows_deletions    = false
+  allows_force_pushes = false
+
+  restrict_pushes {
+    blocks_creations = true
+  }
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+    dismiss_stale_reviews           = true
+    require_last_push_approval      = true
+  }
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build", "Unit Tests"]
+  }
+}
